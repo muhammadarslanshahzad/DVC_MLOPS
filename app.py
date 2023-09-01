7c492f1be0998eac5175b4c1f9911619e0bed419
muhammadarslanshahzad <marslanshahzad14@gmail.com>
2023-09-01 11:41:44 +0500

    finalized api

--- app.py
+++ app.py
@@ -47,33 +47,33 @@ def prediction():
     req_image = request.files['image']
     image_PIL = Image.open(req_image)
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    image_name = req_image.filename.replace(".jpg","")+'_'+current_time+".jpg"
-
+    image_name = req_image.filename.replace(".jpg","")+'_'+current_time+".png"
+   
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.submit(process_image, image_PIL, image_name)
 
     return_image_path = f"http://{host}:{port}/{output_path+image_name}"
     response = {"processed_image_url":return_image_path}
+    print(response)
     return jsonify(response)
 
 
 @app.route("/assets/results/<filename>")
 def serve_image(filename):
     # Replace 'path/to/' with the correct path to your image folder.
-    image_path = os.path.join("assets/results", filename)
-    # print(image_path)
+    image_path = os.path.join("assets/results/", filename)
+    print(image_path)
 
     if not os.path.isfile(image_path):
         # Return an error image or a default image if the requested image doesn't exist.
-        default_image_path = "images/Not_found.jpg"  # Replace with the path to your default image.
+        default_image_path = "assets/error.jpeg"  # Replace with the path to your default image.
         image_path = default_image_path
 
     # Determine the image mimetype based on the file extension.
     # You may need to handle additional image formats as needed.
     mimetype = "image/jpeg" if filename.lower().endswith(".jpg") else "image/png"
-    print('Returning the image')
+    print(f'Returning the image {image_path}')
     return send_file(image_path, mimetype=mimetype)
-
 
 if __name__ == '__main__':
     app.run(host=host, port=port, debug=True)
